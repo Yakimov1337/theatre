858056386075399507cc595d76ce6f463b9cc3f1
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-28 02:15:01 +0300

    Details and delete page

--- controllers/playController.js
+++ controllers/playController.js
@@ -21,7 +21,8 @@ router.post('/create', isUser(), async (req, res) => {
 
         res.redirect('/');
     } catch (err) {
-        console.log(Object.values(err.errors).map(e => e.properties.message));
+        // console.log(Object.values(err.errors).map(e => e.properties.message));
+        console.log(err.message);
         const ctx = {
             errors: [err.message],
             playData: {
@@ -36,4 +37,85 @@ router.post('/create', isUser(), async (req, res) => {
     }
 });
 
+router.get('/details/:id', async (req, res) => {
+    try {
+        const play = await req.storage.getPlayById(req.params.id);
+        play.hasUser = Boolean(req.user);
+        play.isAuthor = req.user && req.user._id == play.author;
+        play.liked = req.user && play.usersLiked.find(u=> u._id ==req.user._id);
+
+        res.render('play/details', { play })
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/404');
+    }
+
+});
+
+router.get('/edit/:id', isUser(), async (req, res) => {
+    try {
+        const play = await req.storage.getPlayById(req.params.id);
+        if (play.author != req.user._id) {
+            throw new Error('Cannot delete play you haven\t created!');
+        }
+        res.render('play/edit', { play });
+    } catch (err) {
+
+    }
+});
+
+router.post('/edit/:id', isUser(), async (req, res) => {
+    try {
+        const play = await req.storage.getPlayById(req.params.id);
+
+        if (play.author != req.user._id) {
+            throw new Error('Cannot delete play you haven\t created!');
+        }
+
+        await req.storage.editPlay(req.params.id, req.body);
+
+        res.redirect('/');
+
+    } catch (err) {
+        const ctx = {
+            errors: [err.message],
+            play: {
+                _id: req.params.id,
+                title: req.body.title,
+                description: req.body.description,
+                imageUrl: req.body.imageUrl,
+                public: Boolean(req.body.public)
+            }
+        };
+        res.render('play/edit', ctx);
+    }
+});
+
+router.get('/delete/:id', isUser(), async (req, res) => {
+    try {
+        const play = await req.storage.getPlayById(req.params.id);
+        if (play.author != req.user._id) {
+            throw new Error('Cannot delete play you haven\t created!');
+        }
+        await req.storage.deletePlay(req.params.id);
+        res.redirect('/');
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/play/details' + req.params.id);
+    }
+});
+
+router.get('/like/:id', isUser(), async(req,res)=>{
+    try {
+        const play = await req.storage.getPlayById(req.params.id);
+        if (play.author== req.user._id) {
+            throw new Error('Cannot like your own play!');
+        }
+        await req.storage.likePlay(req.params.id, req.user._id);
+        res.redirect('/play/details/'+ req.params.id);
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/play/details/' + req.params.id);
+    }
+});
 module.exports = router;
